24779db1b43f2f05d624bc48522f2a155ff38d9e
ohhfishal <ohhfishal@gmail.com>
2025-03-12 10:36:07 -0700

    chore: remove unused variable

--- main.go
+++ main.go
@@ -2,15 +2,11 @@ package main
 
 import (
 	"context"
-	_ "embed"
 	"fmt"
 	"os"
 
 	"github.com/ohhfishal/schedule/cmd"
 )
-
-//go:embed sql/schema.sql
-var schema string
 
 func main() {
 	if err := cmd.Run(context.Background(), os.Stdout, os.Args[1:]); err != nil {
