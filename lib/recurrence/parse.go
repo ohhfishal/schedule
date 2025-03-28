85730d29809dad02e133c0d8de52c6ebeeec820e
ohhfishal <ohhfishal@gmail.com>
2025-03-28 12:36:18 -0700

    dev: implement rrule lexer/parser

--- lib/recurrence/parse.go
+++ lib/recurrence/parse.go
@@ -2,28 +2,34 @@ package recurrence
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/alecthomas/participle/v2"
 	"github.com/alecthomas/participle/v2/lexer"
 )
 
-var _TIME_REGEX = `\d{8}T\d{6}Z`
-
 var (
-	ruleLexer = lexer.MustSimple([]lexer.SimpleRule{
-		{Name: "RRULE", Pattern: `RRULE`},
-		{Name: ":", Pattern: `:`},
-		{Name: ";", Pattern: `;`},
-		{Name: "Keyword", Pattern: `(?i)FREQ|COUNT|UNTIL|INTERVAL|BYMONTH`},
-		{Name: "=", Pattern: `=`},
-		{Name: "Time", Pattern: _TIME_REGEX},
-		{Name: "Frequency", Pattern: `(?i)MINUTELY|HOURLY|DAILY|WEEKLY|MONTHLY|YEARLY`},
-		{Name: "Int", Pattern: `\d+`},
-	})
+	ruleLexerInit sync.Once
+	ruleLexer     lexer.Definition
+	ruleLexerErr  error
 )
 
-type ruleGrammar struct {
-	Parameters []parameter `parser:"'RRULE' ':' @@ (';' @@ )*"`
+func ParseRRule(input string) (*Rule, error) {
+	lexer, err := getRuleLexer()
+	if err != nil {
+		return nil, fmt.Errorf("bad lexer: %w", err)
+	}
+	ruleParser, err := participle.Build[ruleGrammar](
+		participle.Lexer(lexer),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("bad grammar: %w", err)
+	}
+	parsed, err := ruleParser.ParseString("", input)
+	if err != nil {
+		return nil, fmt.Errorf("bad format: %w", err)
+	}
+	return parsed.Rule()
 }
 
 func (rg ruleGrammar) Rule() (*Rule, error) {
@@ -31,35 +37,40 @@ func (rg ruleGrammar) Rule() (*Rule, error) {
 	return nil, nil
 }
 
+type ruleGrammar struct {
+	Parameters []parameter `parser:"'RRULE' ':' @@ (';' @@ )*"`
+}
+
 type parameter struct {
-	Name  string `parser:"@Keyword '='"`
-	Value value  `parser:"@@"`
+	Name  string  `parser:"(@Keyword | @Match) '='"`
+	Value []value `parser:"@@ (',' @@)*"`
 }
 
 type value struct {
 	// TODO: See if I can have a function to convert Time to time.Time
-	Time      *string    `parser:"@Time"`
-	Int       *int       `parser:"| @Int"`
-	Frequency *Frequency `parser:"| @Frequency"`
+	Int       *int       `parser:"@Int?"`
+	Day       *string    `parser:"@Day?"`
+	Time      *string    `parser:"(@Time"`
+	Frequency *Frequency `parser:"| @Frequency)?"`
 }
 
-func ParseRRule(input string) (*Rule, error) {
-	/* Summarized from https://icalendar.org/iCalendar-RFC-5545/3-8-5-3-recurrence-rule.html
-	rrule      = "RRULE" rrulparam ":" recur CRLF
-	rrulparam  = *(";" other-param)
-	*/
-	ruleParser, err := participle.Build[ruleGrammar](
-		participle.Lexer(ruleLexer),
-		// Do we need to look ahead? Maybe for determining type of value?
-		participle.UseLookahead(participle.MaxLookahead),
-	)
-	if err != nil {
-		return nil, fmt.Errorf("bad grammar: %w", err)
-	}
+var _TIME_REGEX = `\d{8}T\d{6}Z`
 
-	parsed, err := ruleParser.ParseString("", input)
-	if err != nil {
-		return nil, fmt.Errorf("bad format: %w", err)
-	}
-	return parsed.Rule()
+func getRuleLexer() (lexer.Definition, error) {
+	ruleLexerInit.Do(func() {
+		ruleLexer, ruleLexerErr = lexer.NewSimple([]lexer.SimpleRule{
+			{Name: "RRULE", Pattern: `RRULE`},
+			{Name: ":", Pattern: `:`},
+			{Name: ",", Pattern: `,`},
+			{Name: ";", Pattern: `;`},
+			{Name: "Keyword", Pattern: `FREQ|COUNT|UNTIL|INTERVAL|WKST`},
+			{Name: "Match", Pattern: `BYYEARDAY|BYWEEKNO|BYSETPOS|BYMONTHDAY|BYMINUTE|BYHOUR|BYDAY|BYMONTH`},
+			{Name: "=", Pattern: `=`},
+			{Name: "Time", Pattern: _TIME_REGEX},
+			{Name: "Frequency", Pattern: `MINUTELY|HOURLY|DAILY|WEEKLY|MONTHLY|YEARLY`},
+			{Name: "Day", Pattern: `SU|MO|TU|WE|TH|FR|SA`},
+			{Name: "Int", Pattern: `-?\d+`},
+		})
+	})
+	return ruleLexer, ruleLexerErr
 }
--- lib/recurrence/rule.go
+++ lib/recurrence/rule.go
@@ -7,10 +7,16 @@ import (
 )
 
 type Frequency string
+type Day string
+type Match string
 
 type Rule struct {
 	Frequency Frequency
 	Count     int
+	Until     time.Time
+	Interval  int
+	WKST      Day // (Enum w/default Monday)
+	By        []Match
 }
 
 func (r *Rule) All(start time.Time) iter.Seq[Rule] {
