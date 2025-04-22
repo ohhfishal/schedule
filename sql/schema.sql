fb58b7c45f58e18be70b5b1ce415362cdcdab7a0
ohhfishal <ohhfishal@gmail.com>
2025-04-22 13:51:44 -0700

    dev: update notes

--- sql/schema.sql
+++ sql/schema.sql
@@ -5,6 +5,8 @@ CREATE TABLE IF NOT EXISTS events (
   description TEXT NOT NULL,
   recurrence JSON,
   -- start_date and end_date for all-day events
+  -- NOTE: This could be a boolean flag that turns that on/off?
+  --       Or two seperate fields?
   start_time INTEGER NOT NULL,
   end_time INTEGER NOT NULL DEFAULT 0
 );
