607d11b42f725c6fdaafca6c87cb728074dfb253
ohhfishal <ohhfishal@gmail.com>
2025-04-22 13:46:34 -0700

    dev: notes for future features

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,8 +1,10 @@
 CREATE TABLE IF NOT EXISTS events (
+  -- PRIMARY KEY (source/user, ID)
   id   INTEGER PRIMARY KEY,
   name TEXT    NOT NULL,
   description TEXT NOT NULL,
   recurrence JSON,
+  -- start_date and end_date for all-day events
   start_time INTEGER NOT NULL,
   end_time INTEGER NOT NULL DEFAULT 0
 );
