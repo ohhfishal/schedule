eae6f0b27c6aa8d0239f55a4ac81814eb6b30d9d
ohhfishal <ohhfishal@gmail.com>
2025-04-28 12:07:58 -0700

    chore: restructure configs to be closer to the code they generate

--- generate.go
+++ generate.go
@@ -2,5 +2,5 @@ package main
 
 // NOTE: To run go generate .
 
-//go:generate go tool oapi-codegen -config api/oapi-codegen.yaml openapi.yaml
+//go:generate go tool oapi-codegen -config api/oapi-codegen.yaml api/openapi.yaml
 //go:generate go tool sqlc generate -f sql/sqlc.yaml
