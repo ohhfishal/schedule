8a4320c979ec819229dac2c301fd1ae8ca190c5e
ohhfishal <ohhfishal@gmail.com>
2025-04-28 13:35:11 -0700

    dev: initial struct

--- api/server.go
+++ api/server.go
@@ -1,27 +1,38 @@
 package api
 
-import "net/http"
+import (
+	"context"
+	"log/slog"
+	"net/http"
+	// "github.com/ohhfishal/schedule/db"
+)
+
+type Database interface {
+	// CreateEvent(ctx context.Context, arg db.CreateEventParams) (db.Event, error)
+}
 
 type Server struct {
+	Logger   *slog.Logger
+	Database Database
 }
 
-func (server Server) getEvents(userId int, params GetEventsParams) http.Handler {
+func (server Server) getEvents(ctx context.Context, userId int, params GetEventsParams) http.Handler {
 	return nil
 }
 
-func (server Server) createEvent(userId int) http.Handler {
+func (server Server) createEvent(ctx context.Context, userId int) http.Handler {
 	return nil
 }
 
-func (server Server) getEventById(userId int, eventId int) http.Handler {
+func (server Server) getEvent(ctx context.Context, userId int, eventId int) http.Handler {
 	return nil
 }
 
-func (server Server) getUserByUsername(w http.ResponseWriter, r *http.Request, username string) http.Handler {
+func (server Server) getUser(ctx context.Context, username string) http.Handler {
 	return nil
 }
 
-func (server Server) putUserByUsername(w http.ResponseWriter, r *http.Request, username string) http.Handler {
+func (server Server) putUser(ctx context.Context, username string) http.Handler {
 	return nil
 }
 
@@ -29,8 +40,6 @@ func (server Server) putUserByUsername(w http.ResponseWriter, r *http.Request, u
 
 // (GET /dev/event/{userId})
 func (server Server) GetEvents(w http.ResponseWriter, r *http.Request, userId int, params GetEventsParams) {
-	handler := server.getEvents(userId, params)
-	handler.ServeHTTP(w, r)
 }
 
 // (POST /dev/event/{userId})
