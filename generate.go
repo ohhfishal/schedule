7e5cb84d03e3aecfd45f02785e74e2e3720f1d7d
ohhfishal <ohhfishal@gmail.com>
2025-04-28 12:01:23 -0700

    chore: move codegen config to their directories

--- generate.go
+++ generate.go
@@ -3,4 +3,4 @@ package main
 // NOTE: To run go generate .
 
 //go:generate go tool oapi-codegen -config api/oapi-codegen.yaml openapi.yaml
-//go:generate go tool sqlc generate
+//go:generate go tool sqlc generate -f sql/sqlc.yaml
